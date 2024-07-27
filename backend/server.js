99112a13b58c53ca3cb1e76e10c4fa9cd6fb9c3d
Your Name <you@example.com>
2024-07-28 03:14:18 +0700

    Connect routes to server

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const experiencesRouter = require('./routes/experiences');
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/experiences', experiencesRouter);
+
 
 mongoose.connect('mongodb://localhost:27017/myportfolio', {
   useNewUrlParser: true,
